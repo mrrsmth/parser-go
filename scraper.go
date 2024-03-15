11bd873d991825c6026d7db051ad8791f3bbb233
alxs1dd93 <alxs1dd93@gmail.com>
2024-03-15 17:16:13 +0700

    Определяем количество страниц для парсинга

--- scraper.go
+++ scraper.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
+
 )
 
 type Quote struct {
@@ -15,31 +16,43 @@ type Quote struct {
 }
 
 func main() {
-	// Отправляем GET-запрос на веб-сайт "https://quotes.toscrape.com"
-	resp, err := http.Get("https://quotes.toscrape.com")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer resp.Body.Close()
+	// Определяем количество страниц для парсинга
+	numPages := 5
 
-	// Загружаем HTML-страницу для парсинга
-	doc, err := goquery.NewDocumentFromReader(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Извлекаем цитаты
 	var quotes []Quote
-	doc.Find(".quote").Each(func(i int, s *goquery.Selection) {
-		text := strings.TrimSpace(s.Find(".text").Text())
-		author := strings.TrimSpace(s.Find(".author").Text())
 
-		quote := Quote{
-			Text:   text,
-			Author: author,
+	// Парсим каждую страницу
+	for page := 1; page <= numPages; page++ {
+		// Формируем URL с номером страницы
+		url := fmt.Sprintf("https://quotes.toscrape.com/page/%d", page)
+
+		// Отправляем GET-запрос на веб-сайт
+		resp, err := http.Get(url)
+		if err != nil {
+			log.Fatal(err)
 		}
-		quotes = append(quotes, quote)
-	})
+
+		// Закрываем тело ответа
+		defer resp.Body.Close()
+
+		// Загружаем HTML-страницу для парсинга
+		doc, err := goquery.NewDocumentFromReader(resp.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Извлекаем цитаты
+		doc.Find(".quote").Each(func(i int, s *goquery.Selection) {
+			text := strings.TrimSpace(s.Find(".text").Text())
+			author := strings.TrimSpace(s.Find(".author").Text())
+
+			quote := Quote{
+				Text:   text,
+				Author: author,
+			}
+			quotes = append(quotes, quote)
+		})
+	}
 
 	// Выводим результат
 	for _, q := range quotes {
